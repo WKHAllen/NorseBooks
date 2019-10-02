78a280feb550e2fc27b6f4eda60bdf0ece3c3a3c
WKHAllen <wkhallen@gmail.com>
2019-10-01 22:04:08 -0500

    Enforce SSL using express-sslify

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const enforce = require('express-sslify');
 const http = require('http');
 const path = require('path');
 
 var app = express();
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 var server = http.Server(app);
 var port = process.env.PORT || 3000;
 server.listen(port, () => {
