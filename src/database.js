f50cdbf2d90af7d75b205ce96aa963fd6cfaea28
WKHAllen <wkhallen@gmail.com>
2019-10-09 11:25:17 -0500

    Add static table populating

--- src/database.js
+++ src/database.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+const csv = require('fast-csv');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 const db = require('./db');
@@ -6,6 +9,7 @@ const dbURL = process.env.DATABASE_URL;
 const saltRounds = 12;
 const hexLength = 64;
 const passwordResetTimeout = 60 * 60 * 1000;
+const staticTablePath = 'tables';
 
 var mainDB = new db.DB(dbURL);
 
@@ -13,7 +17,33 @@ function getTime() {
     return Math.floor(new Date().getTime() / 1000);
 }
 
+function getStaticTablePath(tableName) {
+    return path.join(__dirname, staticTablePath, tableName) + '.csv';
+}
+
+function populateStaticTable(tableName) {
+    var sql;
+    var params;
+    fs.createReadStream(getStaticTablePath(tableName))
+        .pipe(csv.parse({ headers: true }))
+        .on('data', (row) => {
+            console.log(row);
+            sql = `INSERT INTO ? (id, name) VALUES (?, ?);`;
+            params = [tableName, row.id, row.name];
+            mainDB.execute(sql, params);
+        });
+}
+
 function init() {
+    // Drop static tables
+    var dropDepartmentTable = `
+        DROP TABLE IF EXISTS Department;
+    `;
+    var dropBookStateTable = `
+        DROP TABLE IF EXISTS BookState;
+    `;
+    mainDB.executeMany([dropDepartmentTable, dropBookStateTable]);
+    // Create tables
     var userTable = `
         CREATE TABLE IF NOT EXISTS NBUser (
             id SERIAL PRIMARY KEY,
@@ -29,21 +59,15 @@ function init() {
         );
     `;
     var departmentTable = `
-        CREATE TABLE IF NOT EXISTS Department (
+        CREATE TABLE Department (
             id SERIAL PRIMARY KEY,
             name TEXT NOT NULL
         );
     `;
     var bookStateTable = `
-        CREATE TABLE IF NOT EXISTS BookState (
+        CREATE TABLE BookState (
             id SERIAL PRIMARY KEY,
             name TEXT NOT NULL
-        );
-    `;
-    var bookCourseTable = `
-        CREATE TABLE IF NOT EXISTS BookCourse (
-            bookId INT NOT NULL,
-            courseNumber INT NOT NULL
         );
     `;
     var bookTable = `
@@ -59,6 +83,12 @@ function init() {
             stateId INT NOT NULL
         );
     `;
+    var bookCourseTable = `
+        CREATE TABLE IF NOT EXISTS BookCourse (
+            bookId INT NOT NULL,
+            courseNumber INT NOT NULL
+        );
+    `;
     var passwordResetTable = `
         CREATE TABLE IF NOT EXISTS PasswordReset (
             id SERIAL PRIMARY KEY,
@@ -67,8 +97,12 @@ function init() {
             createTimestamp INT NOT NULL
         );
     `;
-    mainDB.executeMany([userTable, departmentTable, bookStateTable, bookCourseTable, bookTable, passwordResetTable]);
-    var sql = `SELECT resetid, createtimestamp FROM passwordReset;`;
+    mainDB.executeMany([userTable, departmentTable, bookStateTable, bookTable, bookCourseTable, passwordResetTable]);
+    // Populate static tables
+    populateStaticTable('BookState');
+    populateStaticTable('Department');
+    // Remove expired password resets
+    var sql = `SELECT resetId, createTimestamp FROM PasswordReset;`;
     mainDB.execute(sql, [], (err, rows) => {
         var timeRemaining;
         for (var row of rows) {
