9ec9dadf343aeae384b944c42e23997fcecb8770
WKHAllen <wkhallen@gmail.com>
2019-10-08 19:17:45 -0500

    Add view engine

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const enforce = require('express-sslify');
-const http = require('http');
-const path = require('path');
+const hbs = require('express-handlebars');
 
 const debug = process.env.PORT === undefined;
 
 var app = express();
 if (!debug)
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
-var server = http.Server(app);
+
+app.engine('.html', hbs({
+    extname: '.html',
+    defaultView: 'default',
+    defaultLayout: 'default'
+}));
+app.set('view engine', '.html');
+
 var port = process.env.PORT || 3000;
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
-var publicDir = 'public';
+app.use(express.static('static'));
 
-app.use(express.static(path.join(__dirname, publicDir)));
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
