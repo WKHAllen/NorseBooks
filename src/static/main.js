e9ff5ad7faf678b55a947ccaa239ab9cc4faf0f7
WKHAllen <wkhallen@gmail.com>
2019-12-02 19:11:18 -0600

    Add book link copying

---
+++ src/static/main.js
@@ -0,0 +1,13 @@
+function showHidden(elementId) {
+    document.getElementById(elementId).classList.remove('hidden');
+}
+
+function copyToClipboard(text) {
+    var copyText = document.createElement('input');
+    copyText.value = text;
+    document.body.appendChild(copyText);
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    document.body.removeChild(copyText);
+}
