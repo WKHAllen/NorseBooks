7e71cf5d02f271d1c852f7a8f50ef8429a97aec6
WKHAllen <wkhallen@gmail.com>
2019-12-15 22:46:35 -0600

    closes #88

--- src/static/js/admin-query.js
+++ src/static/js/admin-query.js
@@ -117,17 +117,23 @@ function hideError() {
     $('#status').addClass('hidden');
 }
 
-// Get the query inputs
-function getQueryInputs() {
-    // Table
-    var table = $('#table').val();
-    // Columns
+// Get the query columns
+function getQueryColumns() {
     var columns = [];
     for (var column of $('#columns .column-checkbox')) {
         if (column.checked) {
             columns.push(column.id.slice(7));
         }
     }
+    return columns;
+}
+
+// Get the query inputs
+function getQueryInputs() {
+    // Table
+    var table = $('#table').val();
+    // Columns
+    var columns = getQueryColumns();
     // Where
     var where = $('#where').val();
     // Where operator
@@ -160,7 +166,7 @@ function buildQuery(queryInputs) {
     var from = `FROM ${queryInputs.table}`;
     var where = '';
     if (queryInputs.where && queryInputs.whereOperator && queryInputs.whereValue) {
-        where = `WHERE ${queryInputs.where} ${queryInputs.whereOperator} ${queryInputs.whereValue}`;
+        where = `WHERE ${queryInputs.where} ${queryInputs.whereOperator} '${queryInputs.whereValue}'`;
     }
     var orderBy = '';
     if (queryInputs.orderBy && queryInputs.orderByDirection) {
@@ -171,9 +177,38 @@ function buildQuery(queryInputs) {
     return query;
 }
 
+// Clear the results table
+function clearResultsTable() {
+    $('#query-results-head').html('');
+    $('#query-results-body').html('');
+}
+
+// Add a row to the results table
+function addResultsRow(rowData, columns) {
+    var resultsBody = $('#query-results-body');
+    resultsBody.append(`<tr></tr>`);
+    var row = resultsBody.children().last();
+    for (var column of columns) {
+        row.append(`<td>${rowData[column]}</td>`);
+    }
+}
+
+// Display the results of the query
+function displayResults(data, columns) {
+    $('#query-results').removeClass('hidden');
+    var resultsHead = $('#query-results-head');
+    for (var column of columns) {
+        resultsHead.append(`<th scope="col">${column}</th>`);
+    }
+    for (var row of data) {
+        addResultsRow(row, columns);
+    }
+}
+
 // Execute the query
 function executeQuery() {
     event.preventDefault();
+    var queryColumns = getQueryColumns();
     var queryInputs = getQueryInputs();
     var query = buildQuery(queryInputs);
     $.ajax({
@@ -182,13 +217,22 @@ function executeQuery() {
         data: { query: query },
         dataType: 'json',
         success: (data) => {
-            console.log(data);
+            clearResultsTable();
+            displayResults(data.result, queryColumns);
         },
         error: (err, req) => {
             showError('Failed to execute the query.');
         }
     });
     return false;
+}
+
+// Update the size of the results table
+function updateTableSize() {
+    if ($('#table-small-check').prop('checked'))
+        $('#query-results-table').addClass('table-sm');
+    else
+        $('#query-results-table').removeClass('table-sm');
 }
 
 // When the page is ready
