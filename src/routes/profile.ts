b44329eed292726b0a582b84a1d3464de87d76cb
WKHAllen <wkhallen@gmail.com>
2020-08-05 19:27:48 -0400

    Implement profile image uploading

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express';
-import { renderPage, Request, Response, auth, stripWhitespace, formatPrice } from './util';
+import { renderPage, Request, Response, auth, stripWhitespace, formatPrice, upload, cloudinaryName, cloudinaryApiKey, cloudinaryApiSecret } from './util';
 import * as services from '../services';
+import * as cloudinary from 'cloudinary';
 import * as owasp from 'owasp-password-strength-test';
+
+cloudinary.v2.config({
+    cloud_name: cloudinaryName,
+    api_key:    cloudinaryApiKey,
+    api_secret: cloudinaryApiSecret
+});
 
 export var router = Router();
 
@@ -64,10 +71,17 @@ router.post('/setName', auth, (req: Request, res: Response) => {
 });
 
 // Set image event
-router.post('/setImage', auth, (req: Request, res: Response) => {
-    services.AuthService.getAuthUser(req.session.sessionId, (userId) => {
-        services.UserService.setUserImage(userId, req.body.imageUrl, () => {
-            res.redirect('/profile');
+router.post('/setImage', auth, upload.single('image'), (req: Request, res: Response) => {
+    cloudinary.v2.uploader.upload(req.file.path, (cloudinaryErr, result) => {
+        services.AuthService.getAuthUser(req.session.sessionId, (userId) => {
+            if (!cloudinaryErr) {
+                services.UserService.setUserImage(userId, result.secure_url, () => {
+                    res.redirect('/profile');
+                });
+            } else {
+                req.session.errorMsg = 'Error uploading image. Please try a different image.';
+                res.redirect('/profile');
+            }
         });
     });
 });
