4a9875e2a7325cf751ca02aa41b5ac710c457ae0
WKHAllen <wkhallen@gmail.com>
2020-08-07 00:25:19 -0400

    Destroy cloudinary images on profile image change

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { renderPage, Request, Response, auth, stripWhitespace, formatPrice, upload, cloudinaryName, cloudinaryApiKey, cloudinaryApiSecret } from './util';
+import { renderPage, Request, Response, auth, stripWhitespace, formatPrice, imagePublicId, logCloudinaryDestroyError, upload, cloudinaryName, cloudinaryApiKey, cloudinaryApiSecret } from './util';
 import * as services from '../services';
 import * as cloudinary from 'cloudinary';
 import * as owasp from 'owasp-password-strength-test';
@@ -72,15 +72,33 @@ router.post('/setName', auth, (req: Request, res: Response) => {
 
 // Set image event
 router.post('/setImage', auth, upload.single('image'), (req: Request, res: Response) => {
-    cloudinary.v2.uploader.upload(req.file.path, (cloudinaryErr, result) => {
-        services.AuthService.getAuthUser(req.session.sessionId, (userId) => {
-            if (!cloudinaryErr) {
-                services.UserService.setUserImage(userId, result.secure_url, () => {
-                    res.redirect('/profile');
+    services.AuthService.getAuthUser(req.session.sessionId, (userId) => {
+        services.UserService.getUserImage(userId, (imageUrl) => {
+            if (imageUrl) {
+                cloudinary.v2.uploader.destroy(imagePublicId(imageUrl), (err, destroyResult) => {
+                    logCloudinaryDestroyError(imageUrl, err, destroyResult);
+                    cloudinary.v2.uploader.upload(req.file.path, (cloudinaryErr, result) => {
+                        if (!cloudinaryErr) {
+                            services.UserService.setUserImage(userId, result.secure_url, () => {
+                                res.redirect('/profile');
+                            });
+                        } else {
+                            req.session.errorMsg = 'Error uploading image. Please try a different image.';
+                            res.redirect('/profile');
+                        }
+                    });
                 });
             } else {
-                req.session.errorMsg = 'Error uploading image. Please try a different image.';
-                res.redirect('/profile');
+                cloudinary.v2.uploader.upload(req.file.path, (cloudinaryErr, result) => {
+                    if (!cloudinaryErr) {
+                        services.UserService.setUserImage(userId, result.secure_url, () => {
+                            res.redirect('/profile');
+                        });
+                    } else {
+                        req.session.errorMsg = 'Error uploading image. Please try a different image.';
+                        res.redirect('/profile');
+                    }
+                });
             }
         });
     });
